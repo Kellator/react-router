f0c450bd8b6195eea515b406395166a0f6cff091
Kellator <k.macleod21@gmail.com>
2017-02-12 15:49:13 -0500

    working contacts list app in react

---
+++ js/components/contact.js
@@ -0,0 +1,17 @@
+// js/components/contact.js
+import React from 'react';
+import {Link} from 'react-router';
+
+export default function Contact(props) {
+    return (
+        <div>
+            <strong>
+            	<Link to={'/contacts/' + props.id}>
+                	{props.name}
+                </Link>
+            </strong>
+            &nbsp;
+            {props.phoneNumber}
+        </div>
+    );
+};
